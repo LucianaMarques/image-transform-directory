87026b76b96bf764cecac6aa287dd8b016854ce7
Luciana da Costa Marques <lucianadacostamarques@gmail.com>
2019-05-27 16:57:55 -0300

    Add arguments to script

--- script.py
+++ script.py
@@ -1,27 +1,72 @@
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
+from PIL import Image
+import argparse
+import os
+from os import listdir
+from os.path import isfile, join
 
 def show_image(image):
     plt.imshow(image, cmap = 'gray', interpolation = 'bicubic')
     plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
     plt.show()
 
+def create_folders(source, destination):
+    source_path = os.path.dirname(source)
+    destination_path = os.path.dirname(destination)
+
+    try:
+        os.stat(source_path)
+    except:
+        os.mkdir(source_path)
+    
+    try:
+        os.stat(destination_path)
+    except:
+        os.stat(destination_path)
+
+def apply_transformation(images, destination, transform):
+    i = 1
+    for img in images:
+        image = cv.imread(img, 0)
+    
+        # Displaying image 
+        cv.imshow('image', image)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
+
+        kernel = np.ones((5,5),np.uint8)
+
+        if (transform == 'erosion'):
+            transf_result = cv.erode(img,kernel,iterations = 1)
+        else:
+            transf_result = cv.dilate(img,kernel,iterations = 1)
+
+        image_name = 'cat'+i+'.png'
+        show_image(transf_result)
+        cv.imwrite(os.path.join(destination, image_name),img)
+        
+        i += 1
+
 def main():
-    img = cv.imread('messi.jpg', 0)
+    parser = argparse.ArgumentParser(description='Process images from a folder and save them to another')
 
-    # Displaying image 
+    parser.add_argument('--origin', dest='org', type=str, required=True)
+    parser.add_argument('--destiny', dest='dest', type=str, required=True)
+    parser.add_argument('--transform', dest='transf', type=str, required=True)
 
-    cv.imshow('image', img)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
+    args = parser.parse_args()
 
-    kernel = np.ones((5,5),np.uint8)
-    erosion = cv.erode(img,kernel,iterations = 1)
-    dilation = cv.dilate(img,kernel,iterations = 1)
+    # if origin and destination folders do not exist, create them
+    create_folders(args.org, args.dest)
 
-    show_image(erosion)
-    show_image(dilation)
+    images = [args.org+img for img in listdir(args.org) if isfile(join(args.org, img))]
+    
+    for img in images:
+        print(img)
+
+    #apply_transformation(images, args.dest, args.transf)
 
 if __name__ == "__main__":
     main()
